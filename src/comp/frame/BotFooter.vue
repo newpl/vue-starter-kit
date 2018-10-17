a5d39d19ef0b8256682755812cf2c851db27e46f
donghashin <shindh159@gmail.com>
2018-10-17 22:08:21 +0900

    Copyright 변경

--- src/comp/frame/BotFooter.vue
+++ src/comp/frame/BotFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="footer">
-        <div class="copyright">Copyright {{year}} SAMSUNG SDS. All rights reserved. </div>
+        <div class="copyright">Made by Dongha, {{year}}. </div>
     </div>
 </template>
 
